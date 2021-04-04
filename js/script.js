0221a3645694d5e5a33f51c8484674114757c2cb
r-evick <r.evick@hotmail.com>
2021-04-04 04:22:09 -0400

    completed code
    completed in visual studio code and tested in various web browsers.

--- js/script.js
+++ js/script.js
@@ -1,75 +1,109 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+Code by: Ryan Evick
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+//the below quotes array stores quotes and other objects.
 
-/*** 
- * `quotes` array 
-***/
 let quotes = [
 {
-  quote: "Quote here",
-  source: "Source here",
-  citation: "Book, movie, or song here",
-  year: "Year here"
+  quote: "No, I am your father.",
+  source: "Darth Vader",
+  citation: "Star Wars: Episode V - The Empire Strikes Back",
+  year: "1980",
+  site: "www.starwars.com"
 },
 {
-  quote: "Quote here",
-  source: "Source here",
-  citation: "Book, movie, or song here",
-  year: "Year here"
+  quote: "Keep your face always toward the sunshine - and shadows will fall behind you.",
+  source: "Walt Whitman",
 },
 {
-  quote: "Quote here",
-  source: "Source here",
-  citation: "Book, movie, or song here",
-  year: "Year here"
+  quote: "The journey of a thousand miles begins with one step.",
+  source: "Lao Tzu",
 },
 {
-  quote: "Quote here",
-  source: "Source here",
-  citation: "Book, movie, or song here",
-  year: "Year here"
+  quote: "There is nothing permanent except change.",
+  source: "Heraclitus",
 },
 {
-  quote: "Quote here",
-  source: "Source here",
-  citation: "Book, movie, or song here",
-  year: "Year here"
+  quote: "Learning never exhausts the mind.",
+  source: "Leonardo da Vinci",
 },
 
 ];
 
+  /*
+  the getRandomQuote function generates a random number from 0 to the # of quotes in the quotes array,
+  stores a random quote pulled from the quotes array,
+  then returns the pulled quote.
+  */
 
-console.log(quotes);
+  function getRandomQuote() {
+    let randomNumber = Math.floor( Math.random() * quotes.length );
+    let randomQuote = quotes[randomNumber];
 
-/***
- * `getRandomQuote` function
-***/
-function getRandomQuote() {
-  // 1. Create a variable that generates a random number
-  // between zero and the last index in the `quotes` array
+    return randomQuote; 
+  }
 
-  // 2. Use the random number variable and bracket notation 
-  // to grab a random object from the `quotes` array, and 
-  // store it in a variable
+/*
+  the below printQuote function calls the above getRandomQuote function in order to
+  use a quote in an html string that will be shown in the browser.
+*/
 
-  // 3. Return the variable storing the random quote object
-  console.log();
+function printQuote() {
+ let randomQuoteObject = getRandomQuote();
+ let html =`
+ <p class="quote">${randomQuoteObject.quote}</p>
+ <p class="source">${randomQuoteObject.source}`
+
+ /*
+   the if statements will check to see if the quote's objects are returned as undefined or not.
+   if not returned as undefined, the quote's objects will be displayed.
+   if returned as undefined, nothing will be displayed for that object.
+ */
+
+ if ( typeof randomQuoteObject.citation !== "undefined" ) {
+   html += `<span class="citation">${randomQuoteObject.citation}</span>`;
+ };
+
+ if ( typeof randomQuoteObject.year !== "undefined" ) {
+  html += `<span class="year">${randomQuoteObject.year}</span>`;
+};
+ 
+if ( typeof randomQuoteObject.site !== "undefined" ) {
+  html += `<span class="site">, <a href="https://www.starwars.com">${randomQuoteObject.site}</a></span>`;
+};
+
+html += `</P>`;  //this is the closing paragraph tag for the above html variable.
+
+document.getElementById('quote-box').innerHTML = html; 
 }
 
-getRandomQuote();
+//the below randomBgColor function generates a random color,
+//then uses it to display as a background color each time the page refreshes.
 
+let red;
+let green;
+let blue;
+let randomRGB;
 
-/***
- * `printQuote` function
-***/
+function randomBgColor () {
+  red = Math.floor(Math.random() * 256);
+  green = Math.floor(Math.random() * 256);
+  blue = Math.floor(Math.random() * 256);
+  randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+  document.querySelector('body').style.background = `${randomRGB}`;
+}
 
+/*
+the below code is used to refresh the random quote and random background color
+at intervals of every 5000 miliseconds (or 5 seconds).
+this code snippet was found at https://www.encodedna.com/javascript/auto-refresh-page-every-10-second-using-javascript-setInterval-method.htm
+*/
 
+setInterval(randomBgColor, 5000);
+setInterval(printQuote, 5000);
 
 /***
  * click event listener for the print quote button
@@ -77,3 +111,8 @@ getRandomQuote();
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+/*
+this code worked as expected when tested in the Google Chrome, Microsoft Edge, and Firefox web browsers.
+this code did not work as expected when tested with Windows 10 Internet Explorer, OS build 19041.867   
+*/
